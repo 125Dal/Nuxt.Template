ac17dac79cd9e143e54d77036c62da2b561d0030
だある <37011352+125Dal@users.noreply.github.com>
2020-11-21 19:18:11 +0900

    :zap: テスト用ディレクティブを追加 (#11)

--- test/Logo.spec.ts
+++ test/Logo.spec.ts
@@ -1,9 +1,10 @@
 import { mount } from '@vue/test-utils'
 import Logo from '@/components/Logo.vue'
+import localVue from '~/test/setup'
 
 describe('Logo', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
-    expect(wrapper.vm).toBeTruthy()
+    const wrapper = mount(Logo, { localVue })
+    expect(wrapper.attributes()['data-testid']).toBe('Logo')
   })
 })
